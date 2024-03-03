824e093f098d87c3217c2c578651133f8042d897
Dells <jv.tandazom@uea.edu.ec>
2024-03-03 10:09:41 -0500

    BIENVENIDO A MI REPOSITORIO

---
+++ Readmi.md.py
@@ -0,0 +1,7 @@
+#Actividad: Clonar un Repositorio GitHub en PyCharm
+
+#En esta actividad se realizo con exito
+#Bienvenidos a mi repositorio
+
+#Frase motivadora
+#¨SI LO PUEDES SOÑAR LO PUEDES PROGRAMAR¨
